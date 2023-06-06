120f2617e0ca65c361cddbed21162bc7eaa1aa7e
unknown <jaiparjapati101@gmail.com>
2023-06-07 01:24:57 +0530

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,302 @@
+/**
+* Template Name: BizPage - v4.0.4
+* Template URL: https://bootstrapmade.com/bizpage-bootstrap-business-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function($) {
+    "use strict";
+  
+    // Preloader
+    $(window).on('load', function() {
+      if ($('#preloader').length) {
+        $('#preloader').delay(100).fadeOut('slow', function() {
+          $(this).remove();
+        });
+      }
+    });
+  
+    // Back to top button
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn('slow');
+      } else {
+        $('.back-to-top').fadeOut('slow');
+      }
+    });
+    $('.back-to-top').click(function() {
+      $('html, body').animate({
+        scrollTop: 0
+      }, 1500, 'easeInOutExpo');
+      return false;
+    });
+  
+    // Smooth scroll for the navigation menu and links with .scrollto classes
+    var scrolltoOffset = $('#header').outerHeight() - 17;
+    $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        if (target.length) {
+          e.preventDefault();
+  
+          var scrollto = target.offset().top - scrolltoOffset;
+  
+          if ($(this).attr("href") == '#header') {
+            scrollto = 0;
+          }
+  
+          $('html, body').animate({
+            scrollTop: scrollto
+          }, 1500, 'easeInOutExpo');
+  
+          if ($(this).parents('.nav-menu, .mobile-nav').length) {
+            $('.nav-menu .active, .mobile-nav .active').removeClass('active');
+            $(this).closest('li').addClass('active');
+          }
+  
+          if ($('body').hasClass('mobile-nav-active')) {
+            $('body').removeClass('mobile-nav-active');
+            $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+            $('.mobile-nav-overly').fadeOut();
+          }
+          return false;
+        }
+      }
+    });
+  
+    // Activate smooth scroll on page load with hash links in the url
+    $(document).ready(function() {
+      if (window.location.hash) {
+        var initial_nav = window.location.hash;
+        if ($(initial_nav).length) {
+          var scrollto = $(initial_nav).offset().top - scrolltoOffset;
+          $('html, body').animate({
+            scrollTop: scrollto
+          }, 1500, 'easeInOutExpo');
+        }
+      }
+    });
+  
+    // Mobile Navigation
+    if ($('.nav-menu').length) {
+      var $mobile_nav = $('.nav-menu').clone().prop({
+        class: 'mobile-nav d-lg-none'
+      });
+      $('body').append($mobile_nav);
+      $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
+      $('body').append('<div class="mobile-nav-overly"></div>');
+  
+      $(document).on('click', '.mobile-nav-toggle', function(e) {
+        $('body').toggleClass('mobile-nav-active');
+        $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+        $('.mobile-nav-overly').toggle();
+      });
+  
+      $(document).on('click', '.mobile-nav .drop-down > a', function(e) {
+        e.preventDefault();
+        $(this).next().slideToggle(300);
+        $(this).parent().toggleClass('active');
+      });
+  
+      $(document).click(function(e) {
+        var container = $(".mobile-nav, .mobile-nav-toggle");
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+          if ($('body').hasClass('mobile-nav-active')) {
+            $('body').removeClass('mobile-nav-active');
+            $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+            $('.mobile-nav-overly').fadeOut();
+          }
+        }
+      });
+    } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
+      $(".mobile-nav, .mobile-nav-toggle").hide();
+    }
+  
+    // Header scroll class
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+        $('#header').addClass('header-scrolled');
+      } else {
+        $('#header').removeClass('header-scrolled');
+      }
+    });
+  
+    if ($(window).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    }
+  
+    // Navigation active state on scroll
+    var nav_sections = $('section');
+    var main_nav = $('.nav-menu, .mobile-nav');
+  
+    $(window).on('scroll', function() {
+      var cur_pos = $(this).scrollTop() + 200;
+  
+      nav_sections.each(function() {
+        var top = $(this).offset().top,
+          bottom = top + $(this).outerHeight();
+  
+        if (cur_pos >= top && cur_pos <= bottom) {
+          if (cur_pos <= bottom) {
+            main_nav.find('li').removeClass('active');
+          }
+          main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
+        }
+        if (cur_pos < 300) {
+          $(".nav-menu ul:first li:first").addClass('active');
+        }
+      });
+    });
+  
+    // Intro carousel
+    var introCarousel = $(".carousel");
+    var introCarouselIndicators = $(".carousel-indicators");
+    introCarousel.find(".carousel-inner").children(".carousel-item").each(function(index) {
+      (index === 0) ?
+      introCarouselIndicators.append("<li data-bs-target='#introCarousel' data-bs-slide-to='" + index + "' class='active'></li>"):
+        introCarouselIndicators.append("<li data-bs-target='#introCarousel' data-bs-slide-to='" + index + "'></li>");
+    });
+  
+    introCarousel.on('slid.bs.carousel', function(e) {
+      $(this).find('h2').addClass('animate__animated animate__fadeInDown');
+      $(this).find('p, .btn-get-started').addClass('animate__animated animate__fadeInUp');
+    });
+  
+    // Skills section
+    $('#skills').waypoint(function() {
+      $('.progress .progress-bar').each(function() {
+        $(this).css("width", $(this).attr("aria-valuenow") + '%');
+      });
+    }, {
+      offset: '80%'
+    });
+  
+    // jQuery counterUp (used in Facts section)
+    $('[data-toggle="counter-up"]').counterUp({
+      delay: 10,
+      time: 1000
+    });
+  
+    // Porfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item',
+      layoutMode: 'fitRows'
+    });
+  
+    $('#portfolio-flters li').on('click', function() {
+      $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+  
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
+      aos_init();
+    });
+  
+    // Initiate venobox (lightbox feature used in portofilo)
+    $(document).ready(function() {
+      $('.venobox').venobox();
+    });
+  
+    // Clients carousel (uses the Owl Carousel library)
+    $(".clients-carousel").owlCarousel({
+      autoplay: true,
+      dots: true,
+      loop: true,
+      responsive: {
+        0: {
+          items: 2
+        },
+        768: {
+          items: 4
+        },
+        900: {
+          items: 6
+        }
+      }
+    });
+  
+    // Testimonials carousel (uses the Owl Carousel library)
+    $(".testimonials-carousel").owlCarousel({
+      autoplay: true,
+      dots: true,
+      loop: true,
+      items: 1
+    });
+  
+    // Portfolio details carousel
+    $(".portfolio-details-carousel").owlCarousel({
+      autoplay: true,
+      dots: true,
+      loop: true,
+      items: 1
+    });
+  
+    // Init AOS
+    function aos_init() {
+      AOS.init({
+        duration: 1000,
+        once: true
+      });
+    }
+    $(window).on('load', function() {
+      aos_init();
+    });
+  
+  })(jQuery);
+  
+  
+  // selling artworks wala page 
+  
+  //selecting all required elements
+  const filterItem = document.querySelector(".items");
+  const filterImg = document.querySelectorAll(".gallery .image");
+  
+  window.onload = ()=>{ //after window loaded
+    filterItem.onclick = (selectedItem)=>{ //if user click on filterItem div
+      if(selectedItem.target.classList.contains("item")){ //if user selected item has .item class
+        filterItem.querySelector(".active").classList.remove("active"); //remove the active class which is in first item
+        selectedItem.target.classList.add("active"); //add that active class on user selected item
+        let filterName = selectedItem.target.getAttribute("data-name"); //getting data-name value of user selected item and store in a filtername variable
+        filterImg.forEach((image) => {
+          let filterImges = image.getAttribute("data-name"); //getting image data-name value
+          //if user selected item data-name value is equal to images data-name value
+          //or user selected item data-name value is equal to "all"
+          if((filterImges == filterName) || (filterName == "all")){
+            image.classList.remove("hide"); //first remove the hide class from the image
+            image.classList.add("show"); //add show class in image
+          }else{
+            image.classList.add("hide"); //add hide class in image
+            image.classList.remove("show"); //remove show class from the image
+          }
+        });
+      }
+    }
+    for (let i = 0; i < filterImg.length; i++) {
+      filterImg[i].setAttribute("onclick", "preview(this)"); //adding onclick attribute in all available images
+    }
+  }
+  
+  //fullscreen image preview function
+  //selecting all required elements
+  const previewBox = document.querySelector(".preview-box"),
+  categoryName = previewBox.querySelector(".title p"),
+  previewImg = previewBox.querySelector("img"),
+  closeIcon = previewBox.querySelector(".icon"),
+  shadow = document.querySelector(".shadow");
+  
+  function preview(element){
+    //once user click on any image then remove the scroll bar of the body, so user cant scroll up or down
+    document.querySelector("body").style.overflow = "hidden";
+    let selectedPrevImg = element.querySelector("img").src; //getting user clicked image source link and stored in a variable
+    let selectedImgCategory = element.getAttribute("data-name"); //getting user clicked image data-name value
+    previewImg.src = selectedPrevImg; //passing the user clicked image source in preview image source
+    categoryName.textContent = selectedImgCategory; //passing user clicked data-name value in category name
+    previewBox.classList.add("show"); //show the preview image box
+    shadow.classList.add("show"); //show the light grey background
+    closeIcon.onclick = ()=>{ //if user click on close icon of preview box
+      previewBox.classList.remove("show"); //hide the preview box
+      shadow.classList.remove("show"); //hide the light grey background
+      document.querySelector("body").style.overflow = "auto"; //show the scroll bar on body
+    }
+  }
